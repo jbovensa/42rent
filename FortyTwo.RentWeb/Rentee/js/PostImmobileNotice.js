40a4f90cef46e1efa228a041444e624a4558107c
jbovensa <jbovensa@gmail.com>
2017-08-24 15:11:01 +0300

    intellisense

--- FortyTwo.RentWeb/Rentee/js/PostImmobileNotice.js
+++ FortyTwo.RentWeb/Rentee/js/PostImmobileNotice.js
@@ -1,11 +1,16 @@
-﻿'use strict';
+﻿/// <reference path="../../js/jquery/3.2.1/jquery.js" />
+/// <reference path="../../js/i18n/jquery.i18n.js" />
+/// <reference path="../../js/i18n/jquery.i18n.messagestore.js" />
+/// <reference path="../../js/i18n/languages/he.js" />
+
+'use strict';
 
 function runI18n() {
 	var i18n = $.i18n();
 
-	i18n.locale = $("#ddlLanguage").val();
+	i18n.locale = $("#ddlLanguage");
 
-	i18n.load('I18N/PostImmobileNotice.' + i18n.locale + '.json', i18n.locale).done(
+	i18n.load('i18n/PostImmobileNotice.' + i18n.locale + '.json', i18n.locale).done(
 		function () {
 			$('body').i18n();
 		});
