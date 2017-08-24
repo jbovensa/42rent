e60b328ee4da571c4cf7d997cec038e36332da60
jbovensa <jbovensa@gmail.com>
2017-08-24 13:27:04 +0300

    i18n

---
+++ FortyTwo.RentWeb/Rentee/JS/PostImmobileNotice.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+function updateText() {
+	var i18n = $.i18n();
+
+	i18n.locale = 'he';
+	i18n.load('I18N/PostImmobileNotice-' + i18n.locale + '.json', i18n.locale).done(
+		function () {
+			$('body').i18n();
+		});
+}
+
+// Enable debug
+$.i18n.debug = true;
+
+$(document).ready(function () {
+	updateText();
+});
