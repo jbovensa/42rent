01edf2d05fafb8ac67d503be67f9fdd9f4f3b193
jbovensa <jbovensa@gmail.com>
2017-08-24 15:20:35 +0300

    add i18n version number

--- FortyTwo.RentWeb/Rentee/js/PostImmobileNotice.js
+++ FortyTwo.RentWeb/Rentee/js/PostImmobileNotice.js
@@ -1,15 +1,20 @@
 ﻿/// <reference path="../../js/jquery/3.2.1/jquery.js" />
-/// <reference path="../../js/i18n/jquery.i18n.js" />
-/// <reference path="../../js/i18n/jquery.i18n.messagestore.js" />
-/// <reference path="../../js/i18n/languages/he.js" />
+/// <reference path="../../js/i18n/1.0.4/jquery.i18n.js" />
+/// <reference path="../../js/i18n/1.0.4/jquery.i18n.messagestore.js" />
+/// <reference path="../../js/i18n/1.0.4/languages/he.js" />
+/// <reference path="../../js/i18n/1.0.4/jquery.i18n.parser.js" />
+/// <reference path="../../js/i18n/1.0.4/jquery.i18n.language.js" />
+/// <reference path="../../js/i18n/1.0.4/jquery.i18n.fallbacks.js" />
+/// <reference path="../../js/i18n/1.0.4/jquery.i18n.emitter.js" />
+/// <reference path="../../js/i18n/1.0.4/jquery.i18n.emitter.bidi.js" />
 
 'use strict';
 
 function runI18n() {
 	var i18n = $.i18n();
 
-	i18n.locale = $("#ddlLanguage");
-
+	i18n.locale = $("#ddlLanguage").val();
+	
 	i18n.load('i18n/PostImmobileNotice.' + i18n.locale + '.json', i18n.locale).done(
 		function () {
 			$('body').i18n();
